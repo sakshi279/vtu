a0dede502ac7593c150c3f42beed45a4a23bd8fa
Sakshi <52036725+sakshi279@users.noreply.github.com>
2022-03-25 00:39:45 +0530

    Update BackPropagation.py

--- BackPropagation.py
+++ BackPropagation.py
@@ -1,6 +1,4 @@
 import numpy as np
-import time
-import matplotlib.pyplot as plt
 X=np.array(([2,9],[1,5],[3,6]),dtype=float)
 y=np.array(([92],[86],[89]),dtype=float)
 X=X/np.amax(X,axis=0)
@@ -10,11 +8,8 @@ def sigmoid(x):
 
 def derivatives_sigmoid(x):
     return x*(1-x)
-A=[]
-B=[]
-for i in range(100):
-    epoch=700*i
-    A.append(epoch) 
+
+    epoch=7000
     lr=0.1
     inputlayer_neurons=2
     hiddenlayer_neurons=3
@@ -25,28 +20,28 @@ for i in range(100):
     wout=np.random.uniform(size=(hiddenlayer_neurons,output_neurons))
     bout=np.random.uniform(size=(1,output_neurons))
 
-    start_time=time.time()
+    
     
 
     for i in range(epoch):
         hinp1=np.dot(X,wh)
-        hinp=hinp1+bh
+        hinp=hinp1 + bh
+        hlayer_act = sigmoid(hinp)
         outinp1=np.dot(hlayer_act,wout)
-        output=sigmoid(outinp)
-        EO=y-output
-        outgrad=derivative_sigmoid(output)
-        d_output=EO* outgrad
-        EH=d_output.dot(wout.T)
-        hiddengrad=derivatives_sigmoid(hlayer_act)
-        d_hiddenlayer=EH*hiddengrad
-        wout+=hlayer_act.T.dot(d_output)*lr
-        bout+=np.sum(d_output,axis=0,keepdims=True)*lr
-        wh+=X.T.dot(d_hiddenlayer)*lr
-        bh+=np.sum(d_hiddenlayer,axis=0,keepdims=True)*lr
-    B.append((time.time() - start_time))
+        outinp= outinp1+ bout
+        output = sigmoid(outinp) 
+        
+        EO = y-output
+        outgrad = derivatives_sigmoid(output)
+        d_output = EO* outgrad
+        EH = d_output.dot(wout.T)
+        hiddengrad = derivatives_sigmoid(hlayer_act)#how much hidden layer wts contributed to
+        d_hiddenlayer = EH * hiddengrad
+        wout += hlayer_act.T.dot(d_output) *lr# dotproduct of nextlayererror and currentlayerop 
+        # bout += np.sum(d_output, axis=0,keepdims=True) *lr
+        wh += X.T.dot(d_hiddenlayer) *lr
+        #bh += np.sum(d_hiddenlayer, axis=0,keepdims=True) *lr
+        print("Input: \n" + str(X))
+        print("Actual Output: \n" + str(y))
+        print("Predicted Output: \n" ,output) 
 
-plt.plot(A, B)
-plt.xlabel('x - axis')
-plt.ylabel('y - axis')
-plt.title('My first graph!')
-plt.show()
